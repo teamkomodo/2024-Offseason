c2fa47c8880d21ee008043d11791fab2466c2dea
ElijahAshw <legos4elijah@gmail.com>
2024-08-31 11:14:47 -0600

    Fix merge problem

--- src/main/java/frc/robot/util/PIDGains.java
+++ src/main/java/frc/robot/util/PIDGains.java
@@ -34,8 +34,4 @@ public class PIDGains {
         this.numArgs = 8;
     }
     
-    public PIDGains(double p, double i, double d, double maxIAccum, double a, double b, double c, double e) {
-        this(0, 0, 0, 0);
-    }
-    
 }
