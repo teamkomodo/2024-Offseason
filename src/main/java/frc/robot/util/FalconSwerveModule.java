ac0d24d051eaa0dba272c16f2aaa1274eea8ae0b
Realtent <realtentwo@gmail.com>
2024-09-21 14:22:50 -0600

    Taking setAngle from something else

--- src/main/java/frc/robot/util/FalconSwerveModule.java
+++ src/main/java/frc/robot/util/FalconSwerveModule.java
@@ -6,8 +6,10 @@ import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.Slot1Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.hardware.core.CoreTalonFX;
 import com.ctre.phoenix6.signals.AbsoluteSensorRangeValue;
 
 import edu.wpi.first.math.MathUtil;
@@ -20,6 +22,7 @@ import edu.wpi.first.networktables.DoubleEntry;
 import edu.wpi.first.networktables.DoublePublisher;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.RobotController;
+import frc.robot.Constants;
 
 import static frc.robot.Constants.*;
 
@@ -31,30 +34,25 @@ public class FalconSwerveModule implements SwerveModule{
     private final double steerPositionConversionFactor = 2 * Math.PI * STEER_REDUCTION / TALON_FX_TICKS_PER_ROTATION; // sensor ticks -> module rotation in radians
     // private final double steerVelocityConversionFactor = steerPositionConversionFactor * 10; // sensor ticks/100ms -> module rad/s
 
-    private final double driveP = 1.0;
-    private final double driveI = 0;
-    private final double driveD = 0;
-
-    private final double steerP = 2.0e-1;
-    private final double steerI = 1.0e-6;
-    private final double steerD = 1.0e-7;
 
     private final TalonFX driveMotor;
 
     private final TalonFX steerMotor;
     private final CANcoder steerAbsoluteEncoder;
 
+    //*Added DutyCycles
     private final DutyCycleOut driveOut = new DutyCycleOut(0);
     private final DutyCycleOut steerOut = new DutyCycleOut(0);
 
+    //*Added Configs
     private final TalonFXConfiguration talonFXConfigs = new TalonFXConfiguration();
     private final Slot0Configs driveConfig = talonFXConfigs.Slot0;
     private final Slot1Configs steerConfig = talonFXConfigs.Slot1;
 
-    private final PIDController driveController;
+    private final PIDController driveController; //moved definition to object initialization
 
     
-    // TODO Measure This
+    //TODO Measure This
     private SimpleMotorFeedforward driveFeedforward = new SimpleMotorFeedforward(0, 3.0); // V/M/s
 
     private SwerveModuleState desiredState;
@@ -63,6 +61,10 @@ public class FalconSwerveModule implements SwerveModule{
     private double relativeSteerAdjustment = 0;
     private double relativeSteerAdjustmentFactor = 0.1;
 
+    private final PositionVoltage anglePosition = new PositionVoltage(0);
+    private Rotation2d lastAngle;
+
+    //*Added Telemetry from NeoSwerveModule
     // Telemetry
     private final DoublePublisher normalizedVelocityError;
     private final DoublePublisher rotationErrorPublisher;
@@ -80,28 +82,34 @@ public class FalconSwerveModule implements SwerveModule{
     private final DoubleEntry steerkIEntry;
     private final DoubleEntry steerkDEntry;
 
+    //*Changed what it takes in to be more in line with NeoSwerveModule
     public FalconSwerveModule(int driveMotorId, int steerMotorId, int steerAbsoluteEncoderId, double steerOffset, PIDGains steerPIDGains, PIDGains drivePIDGains, FFGains driveFFGains, NetworkTable moduleNT) {
         this.driveMotor = new TalonFX(driveMotorId);
         this.steerMotor = new TalonFX(steerMotorId);
+        
         this.steerAbsoluteEncoder = new CANcoder(steerAbsoluteEncoderId);
         this.desiredState = new SwerveModuleState(0.0, Rotation2d.fromRadians(0));
         
-        
+        //*Removed configGetMagnetOffset because the method is depricated
 
+        lastAngle = getState().angle;
+
+        //*Defined driveController and FF
         driveController = new PIDController(drivePIDGains.p, drivePIDGains.i, drivePIDGains.d);
         driveFeedforward = new SimpleMotorFeedforward(driveFFGains.s, driveFFGains.v, driveFFGains.a);
 
         
-
+        //*Configs the steerAbsoluteEncoder as it does in NeoSwerveModule
         steerAbsoluteEncoder.getConfigurator().apply(new MagnetSensorConfigs()
             .withMagnetOffset(steerOffset / (2 * Math.PI))
             .withAbsoluteSensorRange(AbsoluteSensorRangeValue.Signed_PlusMinusHalf)); // CANCoder outputs between (-0.5, 0.5)
 
         
-
+        //*ConfigureMotors works as it does in NeoSwerveModule
         configureMotors(steerPIDGains);
         
         
+        //*Added Telemetry
         // Telemetry
         normalizedVelocityError = moduleNT.getDoubleTopic("normvelocityerror").publish();
         rotationErrorPublisher = moduleNT.getDoubleTopic("rotationerror").publish();
@@ -133,14 +141,21 @@ public class FalconSwerveModule implements SwerveModule{
 
     private void configureMotors(PIDGains steerGains) {
 
+        
+
         //driveMotor.setSensorPhase(true); // invert the sensor
         driveMotor.setInverted(true); // invert the motor
+        //*Removed Neutrol Mode Brake for driveMotor
 
+        //*This is probably from NeoSwerveModule but also is useless
         double wheelPositionConversionFactor = Math.PI * WHEEL_DIAMETER * DRIVE_REDUCTION; // motor rotations -> wheel travel in meters
     
+        //*Removed setSensorPhase as it was depricated
         steerMotor.setInverted(false);
+        //*New way to set braking during Neutral
         steerOut.OverrideBrakeDurNeutral = true;
 
+        //*New way to set PID for steerMotor
         steerConfig.kP = steerGains.p;
         steerConfig.kI = steerGains.i;
         steerConfig.kD = steerGains.d;
@@ -151,6 +166,7 @@ public class FalconSwerveModule implements SwerveModule{
 
     }
 
+    //*Added Telemetry
     public void updateTelemetry(){
         normalizedVelocityError.set((desiredState.speedMetersPerSecond - getDriveVelocity()) * Math.signum(desiredState.speedMetersPerSecond));
         rotationErrorPublisher.set(MathUtil.angleModulus(desiredState.angle.getRadians() - getModuleRotation().getRadians()));
@@ -200,50 +216,73 @@ public class FalconSwerveModule implements SwerveModule{
         return new SwerveModulePosition(getDrivePosition(), getModuleRotation());
     }
 
+    //*Changed setDesiredState to the one used in NeoSwerveModule, which helps with Swerve Jitteriness and Gear Grinding
     public void setDesiredState(SwerveModuleState desiredState) {
         SwerveModuleState optimizedState = SwerveModuleState.optimize(desiredState, getModuleRotation());
         this.desiredState = optimizedState;
 
     }
 
+    //*Removed correctRelativeEncoder
 
+
+    //*Added getValueAsDouble
     public Rotation2d getModuleRotation() {
-        return new Rotation2d(steerMotor.getPosition().getValueAsDouble() + relativeSteerAdjustment);
+        return new Rotation2d(steerAbsoluteEncoder.getPosition().getValueAsDouble() + relativeSteerAdjustment);
         // return new Rotation2d(MathUtil.angleModulus(steerRelativeEncoder.getPosition() + steerOffset + relativeSteerAdjustment)); // Handled by 
     }
 
+    //*Added getValueAsDouble, also set it to same thing as getModuleRotation
     public Rotation2d getAbsoluteModuleRotation() {
-        return new Rotation2d(steerMotor.getPosition().getValueAsDouble() + relativeSteerAdjustment);
+        return new Rotation2d(steerAbsoluteEncoder.getPosition().getValueAsDouble() + relativeSteerAdjustment);
         // return new Rotation2d(MathUtil.angleModulus(Math.toRadians(steerAbsoluteEncoder.getAbsolutePosition()) + steerOffset));
     }
 
     
-    
+    //*Added getValueAsDouble
     private double getDrivePosition() {
         return driveMotor.getRotorPosition().getValueAsDouble() * drivePositionConversionFactor;
     }
 
+    //*Added getValueAsDouble
     private double getDriveVelocity() {
         return driveMotor.getRotorPosition().getValueAsDouble() * driveVelocityConversionFactor;
     }
 
+    private void setAngle(SwerveModuleState desiredState){
+        Rotation2d angle = (Math.abs(desiredState.speedMetersPerSecond) <= (MAX_ATTAINABLE_VELOCITY * 0.01)) ? lastAngle : desiredState.angle; //Prevent rotating module if speed is less then 1%. Prevents Jittering.
 
+        anglePosition.Position = (desiredState.angle.getDegrees() / 180) * 12.8;
+        steerMotor.setControl(anglePosition);
+        lastAngle = angle;
+        lastAngle = desiredState.angle;
+    }
+
+
+
+    //*Added Periodic
     @Override
     public void periodic() {
         final double driveOutput = driveController.calculate(driveMotor.getVelocity().getValueAsDouble(), desiredState.speedMetersPerSecond);
         final double driveFeedforward = this.driveFeedforward.calculate(desiredState.speedMetersPerSecond);
         //System.out.println(driveFeedforward);
         driveMotor.setVoltage(driveOutput + driveFeedforward);
-        //steerMotor.setVoltage((desiredState.angle.getRadians()-steerMotor.getPosition().getValueAsDouble()%5));
-        steerMotor.setPosition(desiredState.angle.getRadians());
+        
+        
+        //steerMotor.setVoltage((desiredState.angle.getRadians()-steerMotor.getPosition().getValueAsDouble()%(2*Math.PI)));
+        setAngle(desiredState);
+
+        //System.out.println(desiredState.angle.getRadians());
     }
 
+    //*Added for SysID
     @Override
     public void runForward(double voltage) {
         driveMotor.setVoltage(voltage);
         steerMotor.setPosition(0);
     }
 
+    //*Added for SysID
     @Override
     public void runRotation(double voltage) {
         driveMotor.set(0);
